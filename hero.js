40c6d7a8ffa63a1dece947dce8f8574af55c1749
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-02-16 16:36:05 -0600

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,24 @@
+class Hero {
+    constructor(x,y,r){
+        var options={
+            density:1,
+            frictionAir:1
+
+        }
+        this.body=Bodies.rectangle(x,y,r,r,options)
+        this.r=r;
+        this.image=loadImage("Superhero-01.png");
+        
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position
+        var angle=this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        imageMode(RADIUS)
+        image(this.image,0,0,this.r,this.r)
+        pop()
+    }
+}
